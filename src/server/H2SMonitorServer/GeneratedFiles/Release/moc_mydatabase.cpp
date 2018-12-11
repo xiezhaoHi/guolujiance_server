d35ede73be40d5be7856f2a99d45edba11c1c57f
xiezhao <15182911195@163.com>
2018-12-11 16:39:04 +0800

    new

--- src/server/H2SMonitorServer/GeneratedFiles/Release/moc_mydatabase.cpp
+++ src/server/H2SMonitorServer/GeneratedFiles/Release/moc_mydatabase.cpp
@@ -1,16 +1,16 @@
 /****************************************************************************
-** Meta object code from reading C++ file 'structs.h'
+** Meta object code from reading C++ file 'mydatabase.h'
 **
 ** Created by: The Qt Meta Object Compiler version 67 (Qt 5.8.0)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
 
-#include "../../../../../server/structs/structs.h"
+#include "../../../../../server/service/db_service/mydatabase.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
-#error "The header file 'structs.h' doesn't include <QObject>."
+#error "The header file 'mydatabase.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
 #error "This file was generated using the moc from 5.8.0. It"
 #error "cannot be used with the include files from this version of Qt."
@@ -20,27 +20,27 @@
 QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
-struct qt_meta_stringdata_CBaseEntity_t {
+struct qt_meta_stringdata_MyDatabase_t {
     QByteArrayData data[3];
-    char stringdata0[31];
+    char stringdata0[29];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
-    qptrdiff(offsetof(qt_meta_stringdata_CBaseEntity_t, stringdata0) + ofs \
+    qptrdiff(offsetof(qt_meta_stringdata_MyDatabase_t, stringdata0) + ofs \
         - idx * sizeof(QByteArrayData)) \
     )
-static const qt_meta_stringdata_CBaseEntity_t qt_meta_stringdata_CBaseEntity = {
+static const qt_meta_stringdata_MyDatabase_t qt_meta_stringdata_MyDatabase = {
     {
-QT_MOC_LITERAL(0, 0, 11), // "CBaseEntity"
-QT_MOC_LITERAL(1, 12, 17), // "entityInfoUpdated"
-QT_MOC_LITERAL(2, 30, 0) // ""
+QT_MOC_LITERAL(0, 0, 10), // "MyDatabase"
+QT_MOC_LITERAL(1, 11, 16), // "Examine_DB_Table"
+QT_MOC_LITERAL(2, 28, 0) // ""
 
     },
-    "CBaseEntity\0entityInfoUpdated\0"
+    "MyDatabase\0Examine_DB_Table\0"
 };
 #undef QT_MOC_LITERAL
 
-static const uint qt_meta_data_CBaseEntity[] = {
+static const uint qt_meta_data_MyDatabase[] = {
 
  // content:
        7,       // revision
@@ -51,60 +51,50 @@ static const uint qt_meta_data_CBaseEntity[] = {
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
-       1,       // signalCount
+       0,       // signalCount
 
- // signals: name, argc, parameters, tag, flags
-       1,    0,   19,    2, 0x06 /* Public */,
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   19,    2, 0x0a /* Public */,
 
- // signals: parameters
+ // slots: parameters
     QMetaType::Void,
 
        0        // eod
 };
 
-void CBaseEntity::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+void MyDatabase::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
     if (_c == QMetaObject::InvokeMetaMethod) {
-        CBaseEntity *_t = static_cast<CBaseEntity *>(_o);
+        MyDatabase *_t = static_cast<MyDatabase *>(_o);
         Q_UNUSED(_t)
         switch (_id) {
-        case 0: _t->entityInfoUpdated(); break;
+        case 0: _t->Examine_DB_Table(); break;
         default: ;
-        }
-    } else if (_c == QMetaObject::IndexOfMethod) {
-        int *result = reinterpret_cast<int *>(_a[0]);
-        void **func = reinterpret_cast<void **>(_a[1]);
-        {
-            typedef void (CBaseEntity::*_t)();
-            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&CBaseEntity::entityInfoUpdated)) {
-                *result = 0;
-                return;
-            }
         }
     }
     Q_UNUSED(_a);
 }
 
-const QMetaObject CBaseEntity::staticMetaObject = {
-    { &QObject::staticMetaObject, qt_meta_stringdata_CBaseEntity.data,
-      qt_meta_data_CBaseEntity,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
+const QMetaObject MyDatabase::staticMetaObject = {
+    { &QObject::staticMetaObject, qt_meta_stringdata_MyDatabase.data,
+      qt_meta_data_MyDatabase,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
 };
 
 
-const QMetaObject *CBaseEntity::metaObject() const
+const QMetaObject *MyDatabase::metaObject() const
 {
     return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
 }
 
-void *CBaseEntity::qt_metacast(const char *_clname)
+void *MyDatabase::qt_metacast(const char *_clname)
 {
     if (!_clname) return Q_NULLPTR;
-    if (!strcmp(_clname, qt_meta_stringdata_CBaseEntity.stringdata0))
-        return static_cast<void*>(const_cast< CBaseEntity*>(this));
+    if (!strcmp(_clname, qt_meta_stringdata_MyDatabase.stringdata0))
+        return static_cast<void*>(const_cast< MyDatabase*>(this));
     return QObject::qt_metacast(_clname);
 }
 
-int CBaseEntity::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+int MyDatabase::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
 {
     _id = QObject::qt_metacall(_c, _id, _a);
     if (_id < 0)
@@ -119,12 +109,6 @@ int CBaseEntity::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
         _id -= 1;
     }
     return _id;
-}
-
-// SIGNAL 0
-void CBaseEntity::entityInfoUpdated()
-{
-    QMetaObject::activate(this, &staticMetaObject, 0, Q_NULLPTR);
 }
 QT_WARNING_POP
 QT_END_MOC_NAMESPACE
